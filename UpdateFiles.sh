f93a3b9a7cabf4f43f685e8dc2f8a8dc14530678
Cvndas <eidasmedia@outlook.com>
2025-08-29 11:51:31 +0200

    Initial Commit ''

---
+++ UpdateFiles.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# --- PATHS ---
+filepath_userPreferences='/Users/eidas/Library/Preferences/Cubase 14/UserPreferences.xml'
+userPreferencesErrorName='Failed to copy UserPreferences.xml'
+
+filepath_keyCommands='/Users/eidas/Library/Preferences/Cubase 14/Key Commands.xml'
+keyCommandsErrorName='Failed to copy Key Commands.xml'
+
+dirpath_projectLogicalEditor='/Users/eidas/Documents/Steinberg/Cubase/User Presets/Project Logical Editor'
+projectLogicalEditorError='Failed to copy project logical editor directory'
+
+dirpath_regularLogicalEditor='/Users/eidas/Documents/Steinberg/Cubase/User Presets/Logical Edit'
+regLogicEditorError='Failed to copy Regular Logical Editor directory'
+
+dirpath_trackPresets='/Users/eidas/Library/Application Support/Steinberg/Track Presets'
+trackPresetsError='Failed to copy track presets directory'
+
+rm -f "$userPreferencesErrorName"
+cp "$filepath_userPreferences" . || touch "$userPreferencesErrorName"
+
+rm -f "$keyCommandsErrorName"
+cp "$filepath_keyCommands" . || touch "$keyCommandsErrorName"
+
+rm -f "$projectLogicalEditorError"
+cp -r "$dirpath_projectLogicalEditor" . || touch "$projectLogicalEditorError"
+
+rm -f "$regLogicEditorError"
+cp -r "$dirpath_regularLogicalEditor" . || touch "$regLogicEditorError"
+
+rm -f "$trackPresetsError"
+cp -r "$dirpath_trackPresets" . || touch "$trackPresetsError"
